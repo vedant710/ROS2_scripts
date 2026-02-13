485124d0190c9e1eccd4113f4f80522405a80b3d
vedant710 <vedantkulkarni252@gmail.com>
2026-02-13 23:57:10 +0530

    added simple static broadcaster

--- src/vedant_py_1/setup.py
+++ src/vedant_py_1/setup.py
@@ -28,6 +28,7 @@ setup(
             'simple_subscriber = vedant_py_1.simple_subscriber:main',
             'simple_parameter = vedant_py_1.simple_parameter:main',
             'simple_turtlesim_kinematics = vedant_py_1.simple_turtlesim_kinematics:main',
+            'simple_tf_kinematics = vedant_py_1.simple_tf_kinematics:main'
         ],
     },
 )
